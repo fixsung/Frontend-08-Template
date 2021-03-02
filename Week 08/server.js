8d27c5d3fcd19795076eeae2b90e7e724eef2b4a
songyzh <songyzh@dist.com.cn>
2021-03-02 16:41:16 +0800

    第八周作业提交

---
+++ Week 08/server.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: songyzh
+ * @Date: 2021-03-01 13:56:42
+ * @LastEditors: songyzh
+ * @LastEditTime: 2021-03-02 16:35:48
+ */
+const http = require("http");
+
+http
+  .createServer((request, response) => {
+    let body = [];
+
+    request
+      .on("error", (error) => {
+        console.log(error);
+      })
+      .on("data", (chunk) => {
+        body.push(chunk.toString());
+      })
+      .on("end", () => {
+        body = body.join("");
+        console.log("body:", body);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end("Hello world\n");
+      });
+  })
+  .listen(8088);
+
+console.log("server started");
